c05cc1be3f06926a83d7ead6b5c963cd7b6ace62
OveDuke <2942366242@qq.com>
2025-08-21 17:22:11 +0800

    321

--- .vitepress/creators.ts
+++ .vitepress/creators.ts
@@ -20,30 +20,6 @@ const getAvatarUrl = (name: string) => `https://github.com/${name}.png`
 
 export const creators: Creator[] = [
   {
-    name: '絢香猫',
-    avatar: '',
-    username: 'nekomeowww',
-    title: '后端开发者',
-    desc: '开发者，专注于基础设施维护，数据分析，后端、DevOps 开发',
-    links: [
-      { type: 'github', icon: 'github', link: 'https://github.com/nekomeowww' },
-    ],
-    nameAliases: ['nekomeowww', '绚香猫', '絢香猫', 'Neko Ayaka', 'Ayaka Neko'],
-    emailAliases: ['neko@ayaka.moe'],
-  },
-  {
-    name: '絢香音',
-    avatar: '',
-    username: 'LittleSound',
-    title: '前段开发者',
-    desc: '开源开发者，专注于前端，以及前端相关工具库和工具链开发',
-    links: [
-      { type: 'github', icon: 'github', link: 'https://github.com/LittleSound' },
-    ],
-    nameAliases: ['LittleSound', '绚香音', '絢香音', 'Rizumu Oikawa', 'Rizumu Ayaka', 'Ayaka Rizumu', 'Rizumu'],
-    emailAliases: ['rizumu@ayaka.moe', 'rizumu@oqo.moe'],
-  },
-  {
     name: '愤怒枇杷',
     avatar: '',
     username: 'OveDuke',
@@ -51,7 +27,7 @@ export const creators: Creator[] = [
     desc: '分享网页中的知识库，负责数字花园的搭建',
     links: [
       { type: 'github', icon: 'github', link: 'https://github.com/OveDuke' },
-      { type: 'yuque', icon: 'yuque', link: 'https://www.yuque.com/ovexds' },
+      { type: 'yuque', icon: 'yuque.svg', link: 'https://www.yuque.com/ovexds' },
     ],
     nameAliases: ['Ove', '欧维', 'Ou Wei'],
     emailAliases: ['ovexds@163.com'],
